570c07a88996c1f859d1a9f8f2a73e1922c66532
Rafael <rafael.martinezcalvo@orange.fr>
2023-01-11 18:13:15 +0100

    ⏰ Ajout de vielles videos

---
+++ compress.bat
@@ -0,0 +1,14 @@
+ffmpeg -y -i .\src\static\2018\video.mp4 -movflags faststart .\src\static\2018\video-b.mp4
+
+
+rm .\src\static\2018\video.mp4 
+mv .\src\static\2018\video-b.mp4 .\src\static\2018\video.mp4 
+
+rm .\src\static\2019\video.mp4 
+mv .\src\static\2019\video-b.mp4 .\src\static\2019\video.mp4 
+
+rm .\src\static\2020\video.mp4 
+mv .\src\static\2020\video-b.mp4 .\src\static\2020\video.mp4 
+
+rm .\src\static\2021\video.mp4 
+mv .\src\static\2021\video-b.mp4 .\src\static\2021\video.mp4 
